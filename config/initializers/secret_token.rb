03d3332c3febf4b4f89d39a51c058063f9bbb251
James Strong <laspluviosillas@gmail.com>
2012-05-25 14:13:26 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TpkDev::Application.config.secret_token = '7da652bfef872a44f79c59afe5d2e1231be75b73332b80ccf7b24cfbae92809b98405043537d18e6a825f9d924f38a7d9b3e9da1c04e7b2430d5f21fa3ec31c6'
