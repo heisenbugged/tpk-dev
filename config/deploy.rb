0a8916cd67b119662be7476c206349d3e2e2ff65
James Strong <laspluviosillas@gmail.com>
2012-09-06 20:29:58 -0500

    adding missing slash to cap deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:laspluviosillas#{application}.git"
+set :repository, "git@github.com:laspluviosillas/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
