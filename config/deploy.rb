65a94ee069c785e8ca08367245f78258a8dd0993
James Strong <laspluviosillas@gmail.com>
2012-09-16 21:51:38 -0500

    changing cap recipe to deploy email.yml file for email sending.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,12 +30,14 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
+    put File.read("config/email.yml.example"), "#{shared_path}/config/email.yml"
     puts "Now edit the config files in #{shared_path}"
   end
   after "deploy:setup", "deploy:setup_config"
   
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/email.yml #{release_path}/config/email.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
   
