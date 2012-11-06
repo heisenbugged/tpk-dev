e91cb38890d78314bd2295870677890d6d36fe4c
James Strong <laspluviosillas@gmail.com>
2012-11-06 16:29:00 -0600

    adding relationships in active admin panel

--- app/admin/it_service.rb
+++ app/admin/it_service.rb
@@ -1,2 +1,26 @@
 ActiveAdmin.register ItService do
+  
+  form do |f|
+    f.buttons        
+    f.inputs "Details" do
+      f.input :name
+    end
+    
+    f.inputs do        
+      f.input :skills, :collection => Skill.all.map { |skill| [skill.name, skill.id] }, :as => :check_boxes
+    end
+    
+    f.buttons
+  end
+
+  show  do
+    attributes_table :name, :created_at, :updated_at
+    panel "Skills" do
+      table_for it_service.skills do
+        column "name" do |s|; s.name; end
+        column "description" do |s|; s.description; end
+      end
+    end    
+  end
+  
 end
--- app/admin/problem_details.rb
+++ app/admin/problem_details.rb
@@ -1,30 +1,30 @@
 ActiveAdmin.register ProblemDetail do
 
   form do |f|
+    f.buttons        
     f.inputs "Details" do
       f.input :name
     end
 
     f.inputs do        
-      f.input :skills, :collection => Skill.all.map { |skill| [skill.name, skill.id] }, :as => :select
+      f.input :problem_category, :collection => ProblemCategory.all.map { |skill| [skill.name, skill.id] }
+    end
+    
+    f.inputs do        
+      f.input :skills, :collection => Skill.all.map { |skill| [skill.name, skill.id] }, :as => :check_boxes
     end
     
     f.buttons
   end
 
   show  do
-    panel "Details" do
-      attributes_table :name, :created_at, :updated_at
-    end
-    
+    attributes_table :name, :created_at, :updated_at, :problem_category
     panel "Skills" do
       table_for problem_detail.skills do
         column "name" do |s|; s.name; end
         column "description" do |s|; s.description; end
-        #column :appointment_date
       end
-    end
-    
+    end    
   end
   
 end
