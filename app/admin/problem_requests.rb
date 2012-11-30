4519ddd2d06926d123063218ee96c782590f899c
James Strong <laspluviosillas@gmail.com>
2012-11-30 14:07:31 -0600

    adding problem details to problem request

--- app/admin/problem_requests.rb
+++ app/admin/problem_requests.rb
@@ -1,2 +1,12 @@
 ActiveAdmin.register ProblemRequest do
+  
+  show  do
+    attributes_table :id, :full_name, :email, :phone, :zip, :description, :latitude, :longitude
+    panel "Problem Details" do
+      table_for problem_request.details do
+        column "name" do |s|; s.name; end
+        column "description" do |s|; s.description; end
+      end
+    end    
+  end  
 end
