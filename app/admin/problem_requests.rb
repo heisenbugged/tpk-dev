695b824bc197620fb49b0d6a6c850786495b9bd0
James Strong <laspluviosillas@gmail.com>
2012-11-30 14:14:16 -0600

    adding actionable link to to problem request details

--- app/admin/problem_requests.rb
+++ app/admin/problem_requests.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register ProblemRequest do
     attributes_table :id, :full_name, :email, :phone, :zip, :description, :latitude, :longitude
     panel "Problem Details" do
       table_for problem_request.details do
-        column "name" do |s|; s.name; end
+        column "name" do |s|; link_to(s.name, admin_problem_detail_path(s)); end
         column "description" do |s|; s.description; end
       end
     end    
